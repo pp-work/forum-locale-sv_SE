431af6696990a57b1e59941ddaa8e014815c51f5
Anders Jensen-Urstad <anders@unix.se>
2014-09-21 05:58:19 +0200

    More appropriate time settings; minor fixes

--- definitions.php
+++ definitions.php
@@ -1,6 +1,6 @@
 <?php
 
- $LocaleInfo['vf_sv_SE'] = array (
+$LocaleInfo['vf_sv_SE'] = array (
   'Locale' => 'sv-SE',
   'Name' => 'Swedish Transifex + PP',
   'Description' => 'Swedish language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/sv_SE/">here</a>. Customized for Piratpartiet <a href="https://github.com/pp-work/forum-locale-sv_SE">here</a>.',
@@ -8,3 +8,5 @@
   'Author' => 'Vanilla Community & Piratpartiet',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/sv_SE/',
 );
+
+setlocale(LC_TIME, 'sv_SE.UTF-8');
