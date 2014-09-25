957506a29193b427579fa1c21741ed5d0289610e
Anders Jensen-Urstad <anders@unix.se>
2014-09-25 03:08:16 +0200

    Add translation for Profile Extender; fix spelling error

--- plugins.php
+++ plugins.php
@@ -3,6 +3,16 @@
 // Flagging
 $Definition['FlagSent'] = 'Ditt klagomål har registrerats.';
 
+// Profile Extender
+$Definition['Profile Fields'] = 'Profilfält';
+$Definition['Birthday'] = 'Födelsedag';
+$Definition['Add Profile Field'] = 'Lägg till profilfält';
+$Definition['Edit Profile Field'] = 'Redigera profilfält';
+$Definition['Birthday Format'] = 'Födelsedagsformat';
+$Definition['One option per line'] = 'Ett alternativ per rad';
+$Definition['Yes'] = 'Ja';
+$Definition['No'] = 'Nej';
+
 // QuoteSelection
 $Definition['%s wrote'] = '%s skrev';
 
