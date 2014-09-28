6f58d02a0c0c589094a72ded5d4512b183ba74ab
Anders Jensen-Urstad <anders@unix.se>
2014-09-28 02:08:28 +0200

    Add more translations for flagging plugin

--- plugins.php
+++ plugins.php
@@ -1,7 +1,19 @@
 <?php
 
 // Flagging
+$Definition[' a comment in'] = "en kommentar i";
+$Definition['%s reported%s <strong>%s</strong>'] = "%s flaggade%s <strong>%s</strong>";
+$Definition['also reported this.'] = "flaggade också det här.";
+$Definition['Flag this!'] = "Flagga!";
+$Definition['FlagDiscuss'] = "Diskutera";
+$Definition['FlagDiscussion'] = "En tråd flaggades.";
+$Definition['FlagForReview'] = "Du är på väg att flagga det här för granskning av moderatorer. Om du är säker på att du vill göra detta, ange en kortfattad anledning och klicka på 'Flag this!'";
+$Definition['FlagLinkContent'] = "Länk till innehåll:";
+$Definition['FlaggedBy'] = "Flaggad av:";
 $Definition['FlagSent'] = 'Ditt klagomål har registrerats.';
+$Definition['Reason'] = "Anledning";
+$Definition['Your complaint has been registered.'] = "Ditt klagomål har registrerats.";
+$Definition['ViewFlagged'] = "isa &raquo;";
 
 // Profile Extender
 $Definition['Profile Fields'] = 'Profilfält';
@@ -18,4 +30,3 @@ $Definition['%s wrote'] = '%s skrev';
 
 // VanillaInThisDiscussion
 $Definition['In this Discussion'] = 'I den här tråden';
-
